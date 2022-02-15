6c5c525d981d6f4c639611ab6a2ee9e16efb7077
jameshwc <jameshwc@gmail.com>
2022-02-15 14:46:16 +0800

    ceiba: [WIP] Download admin module frame

--- ceiba/course_admin.py
+++ ceiba/course_admin.py
@@ -17,34 +17,47 @@ class Admin():
     
     def crawl(self, path: Path):
         self.path = path
-        self.crawl_main_page()
-        # resp = util.get(self.sess, util.admin_module_urls['ftp'])
-        # soup = BeautifulSoup(resp.content, 'html.parser')
-        # self.frame = self.parse_frame(soup, resp.url)
-        # Path("crawl.html").write_text(str(self.frame), encoding='utf-8')
+        modules = self.crawl_main_page()
+        for mod in modules:
+            resp = util.get(self.sess, util.admin_module_urlgen(mod))
+            soup = BeautifulSoup(resp.content, 'html.parser')
+            soup = self.parse_frame(soup, resp.url)
+            mod_path = self.path.joinpath(mod)
+            mod_path.mkdir(exist_ok=True)
+            Crawler(self.sess, resp.url, mod_path).download_css(soup.find_all('link'))
+            mod_path.joinpath(mod+".html").write_text(str(soup), encoding='utf-8')
     
-    def crawl_main_page(self):
+    def crawl_main_page(self) -> List[str]:
         resp = util.get(self.sess, self.admin_url + self.course_sn)
         soup = BeautifulSoup(resp.content, 'html.parser')
+        modules = []
         link_button: Tag
         for link_button in soup.find_all('input'):
+            if link_button.get('type') == 'hidden':
+                continue
             if link_button.get('value') == '進入' and link_button.get('disabled') == None:
-                module = re.search(r"singleadm\('([a-z]*)'\)" ,link_button.get('onclick')).group(1)
-                link_button.name = 'a'
-                link_button['href'] = module + "/" + module + ".html"
                 link_button.string = link_button.get('value')
+                link_button.name = 'a'
+                module = re.search(r"singleadm\('([a-z]*)'\)" ,link_button.get('onclick')).group(1)
+                link_button['href'] = module + "/" + module + ".html"
+                modules.append(module)
+            else:
+                link_button.replaceWithChildren()
         Crawler(self.sess, resp.url, self.path).download_css(soup.find_all('link'))
         self.path.joinpath('index.html').write_text(str(soup), encoding='utf-8')
+        return modules
     
-    def parse_frame(self, soup: BeautifulSoup, url: str) -> List[str]:
+    def parse_frame(self, soup: BeautifulSoup, url: str) -> BeautifulSoup:
         nav = soup.find('div', {"id": "majornav"})
         module_urls = []
         a: Tag
         for a in nav.find_all('a'):
-            if a.get_text() in ['主功能表', '主題首頁', '']:
-                continue
             module_urls.append((a.get_text(), urljoin(url, a['href'])))
-            a['href'] = util.admin_ename_map[a.get_text()]
+            if a.get_text() == '主功能表':
+                a['href'] = "../index.html"
+            else:
+                module = util.admin_ename_map[a.get_text()]
+                a['href'] = "../" + module + "/" + module + ".html"
         
         for a in soup.find('ul', {"id": "nav-top"}).find_all('a'):
             a.replaceWithChildren()
